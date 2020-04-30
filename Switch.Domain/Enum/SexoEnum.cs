01fe10c18470d419ebeed7ed71cce598c4695ed9
carlos sergio <csergio120605@gmail.com>
2020-04-30 19:46:41 -0300

    pacotes do entity e pastas

---
+++ Switch.Domain/Enum/SexoEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Domain.Enum
+{
+    public enum SexoEnum
+    {
+        Masculino = 1,
+        Feminino = 2,
+        NaoDefenido = 3
+    }
+} 
